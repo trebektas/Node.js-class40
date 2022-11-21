083fee322aa703d74bcfd5aaa181b15de286cc0d
trebektas <trensarbektas@gmail.com>
2022-11-21 22:05:30 +0100

    Add external API

--- homework/hackyourtemperature/server.js
+++ homework/hackyourtemperature/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 // import { engine } from 'express-handlebars';
-// import fetch from 'node-fetch';
+import fetch from 'node-fetch';
+import { apiKey } from './sources/keys.js';
 
 const app = express();
 
@@ -10,9 +11,18 @@ app.get('/', (req, res) => {
   res.send('Hello from backend to frontend!');
 });
 
-app.post('/weather', (req, res) => {
+app.post('/weather', async (req, res) => {
   const cityName = req.body.cityName;
-  res.json(cityName);
+  try {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=${apiKey.API_KEY}`);
+    const data = await response.json();
+    console.log(data);
+    const temp = data.main.temp
+    res.json({cityName, temp})
+  } catch (error) {
+    console.log(error); 
+    res.json({ weatherText: "City is not found!" });
+  }
 });
 
 app.listen(3000);
---
+++ homework/hackyourtemperature/sources/keys.js
@@ -0,0 +1,1 @@
+export const apiKey = { API_KEY: "2d521ae7e5a549004a32e076ea3ca4d2" }
